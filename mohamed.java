0da8905ac3aecddc6c6af23f8dd5fe3418e13bc7
AymanAlqoqa <aalqouqa@gmail.com>
2023-03-25 11:14:52 +0300

    Initial commit

---
+++ mohamed.java
@@ -0,0 +1,14 @@
+public class mohamed {
+    public static void main(string [] args){
+        string m=("adnan");
+        system.out.print(m);
+
+
+
+
+    }
+
+
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class Main {
+    public static void main(String[] args) {
+
+        JOptionPane.showMessageDialog(null,"welcome to java");
+        System.exit(0);
+    }
+}
